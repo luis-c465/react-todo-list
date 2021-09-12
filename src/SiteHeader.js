ea18a3aa44978bc26393b55829a7820c93c7b431
Luis Canada <canadaluis5@gmail.com>
2021-09-12 19:12:02 -0400

    Add React logo

--- src/SiteHeader.js
+++ src/SiteHeader.js
@@ -1,12 +1,19 @@
 import React from "react";
 import SiteHeaderItem from "./SiteHeaderItem";
 import "./SiteHeader.css";
+import reactLogo from "./logo.svg"
 
 class SiteHeader extends React.Component{
     render() {
         return (
             <header className="SiteHeader">
                 <div>
+                    <img
+                        className="logo"
+                        src={ reactLogo }
+                        alt="React Logo"
+                    >
+                    </img>
                     <a
                         href={ window.location.href }
                     >
