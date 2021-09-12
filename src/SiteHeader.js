b9728cc03dd5e3a5156d406ee41340d17d123865
Luis Canada <canadaluis5@gmail.com>
2021-09-12 16:19:24 -0400

    Convert to react component
    Add props.onclick to create new todo.

--- src/SiteHeader.js
+++ src/SiteHeader.js
@@ -1,28 +1,28 @@
+import React from "react";
 import SiteHeaderItem from "./SiteHeaderItem";
 import "./SiteHeader.css";
 
-function SiteHeader() {
-    return (
-        <header className="SiteHeader">
-            <div>
-                <a
-                    href={ window.location.href }
-                >
-                    React Todo-list
-                </a>
-            </div>
-            <ol className="SiteHeaderContainer">
-                <SiteHeaderItem
-                    value="Create new todo"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        console.log("amogus");
-                    }}
-                    href=""
-                />
-            </ol>
-        </header>
-    )
+class SiteHeader extends React.Component{
+    render() {
+        return (
+            <header className="SiteHeader">
+                <div>
+                    <a
+                        href={ window.location.href }
+                    >
+                        React Todo-list
+                    </a>
+                </div>
+                <ol className="SiteHeaderContainer">
+                    <SiteHeaderItem
+                        value="Create new todo"
+                        onClick={ () => this.props.addTodo() }
+                        href=""
+                    />
+                </ol>
+            </header>
+        )
+    }
 }
 
 export default SiteHeader;
