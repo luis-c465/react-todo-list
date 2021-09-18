3783d59990bfcd409779b83107df21aa726af637
Luis Canada <canadaluis5@gmail.com>
2021-09-18 18:52:22 -0400

    Convert to functional component

--- src/SiteHeader.js
+++ src/SiteHeader.js
@@ -1,35 +1,30 @@
-import React from "react";
 import SiteHeaderItem from "./SiteHeaderItem";
 import "./SiteHeader.css";
 import reactLogo from "./logo.svg"
 
-class SiteHeader extends React.Component{
-    render() {
-        return (
-            <header className="site-header">
-                <div>
-                    <img
-                        className="logo"
-                        src={ reactLogo }
-                        alt="React Logo"
-                    >
-                    </img>
-                    <a
-                        href={ window.location.href }
-                    >
-                        React Todo-list
-                    </a>
-                </div>
-                <ol className="SiteHeaderContainer">
-                    <SiteHeaderItem
-                        value="Create new todo"
-                        onClick={ () => this.props.addTodo() }
-                        href=""
-                    />
-                </ol>
-            </header>
-        )
-    }
+export default function SiteHeader(props) {
+    return (
+        <header className="site-header">
+            <div>
+                <img
+                    className="logo"
+                    src={ reactLogo }
+                    alt="React Logo"
+                >
+                </img>
+                <a
+                    href={ window.location.href }
+                >
+                    React Todo-list
+                </a>
+            </div>
+            <ol className="SiteHeaderContainer">
+                <SiteHeaderItem
+                    value="Create new todo"
+                    onClick={ () => props.addTodo() }
+                    href=""
+                />
+            </ol>
+        </header>
+    )
 }
-
-export default SiteHeader;
