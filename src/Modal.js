c948b0cc0ceb240f4adcb6356977a20572e5e0da
Luis Canada <canadaluis5@gmail.com>
2021-09-18 18:55:10 -0400

    Convert to functional component

--- src/Modal.js
+++ src/Modal.js
@@ -1,51 +1,44 @@
-import React from "react";
 import "./Modal.css";
 
-export default class Modal extends React.Component {
-    render() {
-        if (!this.props.show) {
-            return null;
-        }
-
-        const closeOnEscapeDown = (e) => {
-            if ((e.charCode || e.keyCode) === 27) {
-                this.props.onClose()
-            }
-        }
-
-        document.body.addEventListener("keydown", (e) => {
-            closeOnEscapeDown(e);
-            document.body.removeEventListener("keydown", closeOnEscapeDown)
-        });
-
-        return (
-            <div className="modal" onClick={this.props.onClose}>
-                <div className="modal-content"
-                    onClick={e => e.stopPropagation()}
-                >
-
-                    <div className="modal-header">
-                        <h4 className="modal-title">{ this.props.title }</h4>
-                    </div>
-
-                    <div className="modal-body">
-                        { this.props.children }
-                    </div>
-
-                    <div className="modal-footer">
-                        <button
-                            onClick={this.props.onClose}
-                            className="button"
-                        >
-                            Close
-                        </button>
-                    </div>
-
-                </div>
-            </div>
-        )
+export default function Modal(props) {
+    if (!props.show) {
+        return null;
     }
 
-    // showModal = () => this.setState({ show: true });
-    // hideModal = () => this.setState({ show: false });
+    const closeOnEscapeDown = (e) => {
+        if ((e.charCode || e.keyCode) === 27) {
+            props.onClose()
+        }
+    }
+    document.body.addEventListener("keydown", (e) => {
+        closeOnEscapeDown(e);
+        document.body.removeEventListener("keydown", closeOnEscapeDown)
+    });
+
+    return (
+        <div className="modal" onClick={props.onClose}>
+            <div className="modal-content"
+                onClick={e => e.stopPropagation()}
+            >
+
+                <div className="modal-header">
+                    <h4 className="modal-title">{ props.title }</h4>
+                </div>
+
+                <div className="modal-body">
+                    { props.children }
+                </div>
+
+                <div className="modal-footer">
+                    <button
+                        onClick={props.onClose}
+                        className="button"
+                    >
+                        Close
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    )
 }
