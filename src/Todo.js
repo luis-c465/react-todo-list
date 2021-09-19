0f6778cbf2b805f671be05a0a38bfc8084f4efd9
Luis Canada <canadaluis5@gmail.com>
2021-09-19 16:09:00 -0400

    Add todo remove button styles

--- src/Todo.js
+++ src/Todo.js
@@ -3,16 +3,16 @@ import "./Todo.css";
 export default function Todo(props) {
     return (
         <div className="todo">
-            <h2>{ props.title }</h2>
-            <p>{ props.content }</p>
-            <h4>Date by: { props.dateBy }</h4>
-            <h4>Date added: { props.dateAdded }</h4>
-
             <button onClick={props.remove}
                 className="remove-button"
             >
                 [X]
             </button>
+            <h2>{ props.title }</h2>
+            <p>{ props.content }</p>
+            <h4>Date by: { props.dateBy }</h4>
+            <h4>Date added: { props.dateAdded }</h4>
+
         </div>
     )
 }
