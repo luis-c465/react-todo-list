d60dd2b7d7271e71845f3d76128e67a0b3f88c0a
Luis Canada <canadaluis5@gmail.com>
2021-09-19 16:08:17 -0400

    Add remove button to todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,14 @@
 import Todo from "./Todo";
 
 export default function TodoList(props) {
-    const todoList = props.todo.map(todo => {
+    const todoList = props.todo.map((todo, index) => {
         return (
             <Todo
                 title={todo.title}
                 content={todo.content}
                 dateBy={todo.dateBy.toLocaleString()}
                 dateAdded={todo.dateAdded.toLocaleString()}
+                remove={() => props.removeTodo(index)}
                 key={+todo.dateAdded}
             />
         )
